5cf865dd5d91c74cca401bfbc9169d3b4e4929b9
yuya-yuya <yytyfp5115@icloud.com>
2021-06-10 14:05:38 +0900

    aaditional commit

--- resources/views/users/unsubscribe.blade.php
+++ resources/views/users/unsubscribe.blade.php
@@ -1,4 +1,4 @@
-<form method="POST" action="http://127.0.0.1:8000/users/{{ $user->id }}/withdraw">
+<form method="POST" action="{{ route('users.withdraw', ['id' => $user->id]) }}">
   @csrf
   <button type="submit">削除</button>
 </form>
