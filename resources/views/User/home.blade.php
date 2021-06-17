09e33635709936c4c6f23cba001929f3dae15278
yuya-yuya <yytyfp5115@icloud.com>
2021-06-18 05:53:24 +0900

    additional commit

--- resources/views/User/home.blade.php
+++ resources/views/User/home.blade.php
@@ -11,6 +11,16 @@
       </div>
     @endforeach
   </ul>
+
+  <div class="container">
+    <div class="theTarget">
+      <div style="background-image: url(img/test1.jpg)"></div>
+      <div style="background-image: url(img/test2.jpg)"></div>
+      <div style="background-image: url(img/test3.jpg)"></div>
+      <div style="background-image: url(img/test4.jpg)"></div>
+      <div style="background-image: url(img/test5.jpg)"></div>
+    </div>
+    </div>
 @endsection
 
 
--- resources/views/layouts/user/app.blade.php
+++ resources/views/layouts/user/app.blade.php
@@ -11,6 +11,8 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/skippr.min.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/8f0be0a856.js" crossorigin="anonymous"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -18,81 +20,94 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/skippr.css') }}" rel="stylesheet">
+
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @unless (Auth::guard('user')->check())
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.home.index') }}">トップページ</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.login') }}">ログイン</a>
-                            </li>
-                            @if (Route::has('user.register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('user.register') }}">新規登録</a>
-                                </li>
-                            @endif
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.users.index')}}">ユーザー一覧</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.posts.index')}}">投稿一覧</a>
-                            </li> 
-                        @else
-                            @if (Auth::guard('admin')->check())
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('admin.home.index') }}">管理者トップ</a>
-                                </le>
-                            @endif
-                            <li class="nav-item">
-                                <p class="nav-link">ようこそ{{ Auth::user()->name }}さん</p>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.posts.new')}}">新規投稿</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.users.index')}}">ユーザー一覧</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.posts.index')}}">投稿一覧</a>
-                            </li> 
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('user.logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    ログアウト
-                                </a>
-                                <form id="logout-form" action="{{ route('user.logout') }}" method="POST">
-                                    @csrf
-                                </form>
-                            </li>
-                        @endunless
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+<!-- Authentication Links -->
+@unless (Auth::guard('user')->check())
+    <nav class="navbar bg-dark  navbar-dark fixed-top">
+        <p class="navbar-brand">転・点・展</p>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.home.index') }}">
+                        <i class="fas fa-home">ホーム</i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.login') }}">
+                        <i class="fas fa-sign-in-alt">ログイン</i>
+                    </a>
+                </li>
+                @if (Route::has('user.register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('user.register') }}">
+                            <i class="fas fa-external-link-alt">新規登録</i>
+                        </a>
+                    </li>
+                @endif
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.users.index')}}">
+                        <i class="fas fa-user-friends">ユーザー一覧</i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.posts.index')}}">
+                        <i class="fas fa-book">投稿一覧</i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+@else
+    <nav class="navbar bg-dark  navbar-dark fixed-top">
+        <p class="navbar-brand">転・点・展</p>
+        <p class="nav-link" style="color: white;">ようこそ{{ Auth::user()->name }}さん</p>
+        @if (Auth::guard('admin')->check())
+            <a class="nav-link" href="{{ route('admin.home.index') }}">管理者トップ</a>
+        @endif
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.posts.new')}}">
+                        <i class="fas fa-external-link-alt">新規登録</i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.users.index')}}">
+                        <i class="fas fa-user-friends">ユーザー一覧</i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.posts.index')}}">
+                        <i class="fas fa-book">投稿一覧</i>
+                    </a>
+                </li> 
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('user.logout') }}" onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt">ログアウト</i>
+                    </a>
+                    <form id="logout-form" action="{{ route('user.logout') }}" method="POST">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
+        </div>
+    </nav>
+@endunless
+<br>
+<br>
+<br>
+<main class="py-4">
+    @yield('content')
+</main>
 </body>
 </html>
